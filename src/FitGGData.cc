9b79ffe921ee741984f553d9b0e7f93a4b690ee6
crovelli <>
2013-07-22 08:42:42 +0000

    extra added

--- src/FitGGData.cc
+++ src/FitGGData.cc
@@ -1,7 +1,8 @@
 MLOptions GetDefaultOptions() {
   MLOptions opts;
   // Fit configuration
-  opts.addBoolOption("dataFit",         "Fit all species",                kTRUE);
+  opts.addBoolOption("dataFit",         "Fit all species",                kFALSE);
+  opts.addBoolOption("sgnOnlyFit",      "Fit signal only",                kTRUE);
   opts.addBoolOption("bkgOnlyFit",      "Fit bkg species only",           kFALSE);
   opts.addBoolOption("weightedDataset", "use event weight instead of 1",  kFALSE);
 
@@ -18,6 +19,7 @@ RooDataSet *getDataSet(const char *rootfile, const char *treename) {
   // fit variables
   RooRealVar* massggnewvtx = new RooRealVar("massggnewvtx", "m_{#gamma #gamma} [GeV]", 100., 180.);
   RooRealVar* mjj          = new RooRealVar("mjj", "m_{jet jet} [GeV]", 0., 300.);
+  RooRealVar* mggjj        = new RooRealVar("mggjj", "m_{#gamma #gammajet jet} [GeV]", 200., 500.); //chiara!
   
   // needed bools
   RooRealVar* btagCategory = new RooRealVar("btagCategory", "btagCategory", 0.9,2.1);
@@ -25,13 +27,13 @@ RooDataSet *getDataSet(const char *rootfile, const char *treename) {
   // weights
   RooRealVar* weight = new RooRealVar("weight", "weight",0,10.);
 
-  RooArgSet setall(*massggnewvtx,*mjj,*btagCategory,*weight);
+  RooArgSet setall(*massggnewvtx,*mjj,*mggjj,*btagCategory,*weight);            
 
   TFile *file = TFile::Open(rootfile);
   TTree *tree = (TTree*)file->Get(treename);
 
-  // RooDataSet *data = new RooDataSet("T1","dataset",tree,setall,0,weight->GetName());     // chiara!
-  RooDataSet *data = new RooDataSet("myTrees","dataset",tree,setall,0);
+  RooDataSet *data = new RooDataSet("T1","dataset",tree,setall,0,weight->GetName());     // chiara!
+  // RooDataSet *data = new RooDataSet("myTrees","dataset",tree,setall,0);
 
   data->Print();
 
@@ -51,9 +53,11 @@ void FitGG() {
   // define the structure of the dataset
   RooRealVar* massggnewvtx = new RooRealVar("massggnewvtx", "m_{#gamma #gamma} [GeV]", 100., 180.);
   RooRealVar* mjj          = new RooRealVar("mjj",          "m_{jet jet} [GeV]",       0., 300.);
+  RooRealVar* mggjj        = new RooRealVar("mggjj", "m_{#gamma #gammajet jet} [GeV]", 200., 500.); //chiara!
   RooRealVar* btagCategory = new RooRealVar("btagCategory", "btagCategory",           -0.9,2.1);
   theFit.AddFlatFileColumn(massggnewvtx);
   theFit.AddFlatFileColumn(mjj);
+  theFit.AddFlatFileColumn(mggjj);
   theFit.AddFlatFileColumn(btagCategory);
 
   // to restrict to sidebands 
@@ -69,19 +73,25 @@ void FitGG() {
   theFit.addSpecies("myFit", "bkg", "Bkg   Component");
   
   // mgg PDF - signal
-  theFit.addPdfWName("myFit", "sig" , "massggnewvtx",  "Cruijff",  "sig_Mass");          
+  // theFit.addPdfWName("myFit", "sig" , "massggnewvtx",  "Cruijff",  "sig_Mass");          
+  theFit.addPdfWName("myFit", "sig" , "mggjj",  "Totti",  "sig_Mass");          
 
   // mLL PDF - background
-  theFit.addPdfWName("myFit", "bkg" , "massggnewvtx",  "Expo", "bkg_Mass");    
+  // theFit.addPdfWName("myFit", "bkg" , "massggnewvtx",  "Expo", "bkg_Mass");    
+  theFit.addPdfWName("myFit", "bkg" , "mggjj",  "Expo", "bkg_Mass");   // chiara!   
 
   // Load the data
   char datasetnameB[200];
-  sprintf(datasetnameB, "../HHggbb/finalizedTrees_Radion_codiceCommittato/Radion_Data2012_default_CSV.root");  
+  sprintf(datasetnameB, "../HHggbb/finalizedTrees_Radion_presel/Radion_Data2012_default_CSV.root");  
+
+  char datasetnameS[200];
+  sprintf(datasetnameS, "../HHggbb/finalizedTrees_Radion_presel/Radion_Radion_M-300_madgraph_default_CSV.root");
 
   char treename[100];
   sprintf(treename,"myTrees");   
 
   RooDataSet *data;
+  if(opts.getBoolVal("sgnOnlyFit")) data = getDataSet(datasetnameS,  treename);
   if(opts.getBoolVal("bkgOnlyFit")) data = getDataSet(datasetnameB,  treename);
   if(opts.getBoolVal("dataFit"))    data = getDataSet(datasetnameB,  treename);
 
@@ -92,6 +102,7 @@ void FitGG() {
   
   // Initialize the fit
   char fitconfig[200];
+  if(opts.getBoolVal("sgnOnlyFit"))    sprintf(fitconfig,"fitconfig/fitSignal.config");    
   if(opts.getBoolVal("bkgOnlyFit"))    sprintf(fitconfig,"fitconfig/fitBackground.config");    
   if(opts.getBoolVal("dataFit"))       sprintf(fitconfig,"fitconfig/fitData.config");    
   theFit.initialize(fitconfig);
@@ -101,7 +112,7 @@ void FitGG() {
   myPdf->getParameters(data)->selectByAttrib("Constant",kFALSE)->Print("V");
 
   RooFitResult *fitres =  myPdf->fitTo(*data,
-				       RooFit::Range("R1,R2"),   // chiara
+				       // RooFit::Range("R1,R2"),   // chiara
                                        RooFit::ConditionalObservables(theFit.getNoNormVars("myFit")),
                                        RooFit::FitOptions("MHTER"),
                                        RooFit::NumCPU(4));
@@ -109,6 +120,7 @@ void FitGG() {
 
   // write the config file corresponding to the fit minimum
   char configfilename[200];
+  if(opts.getBoolVal("sgnOnlyFit"))    theFit.writeConfigFile("fitres/fitSignal.config");  
   if(opts.getBoolVal("bkgOnlyFit"))    theFit.writeConfigFile("fitres/fitBackground.config");  
   if(opts.getBoolVal("dataFit"))       theFit.writeConfigFile("fitres/fitData.config");    
 }
@@ -125,9 +137,11 @@ void PlotGG(int nbins=19) {
   // define the structure of the dataset
   RooRealVar* massggnewvtx = new RooRealVar("massggnewvtx", "m_{#gamma #gamma} [GeV]", 100., 180.);
   RooRealVar* mjj          = new RooRealVar("mjj",          "m_{jet jet} [GeV]",       0., 300.);
+  RooRealVar* mggjj        = new RooRealVar("mggjj", "m_{#gamma #gammajet jet} [GeV]", 200., 500.); //chiara!
   RooRealVar* btagCategory = new RooRealVar("btagCategory", "btagCategory",           -0.9,2.1);
   theFit.AddFlatFileColumn(massggnewvtx);
   theFit.AddFlatFileColumn(mjj);
+  theFit.AddFlatFileColumn(mggjj);
   theFit.AddFlatFileColumn(btagCategory);
 
   // to restrict to sidebands
@@ -143,21 +157,26 @@ void PlotGG(int nbins=19) {
   theFit.addSpecies("myFit", "bkg", "Bkg   Component");
   
   // mgg PDF - signal
-  theFit.addPdfWName("myFit", "sig" , "massggnewvtx",  "Cruijff",  "sig_Mass");
+  // theFit.addPdfWName("myFit", "sig" , "massggnewvtx",  "Cruijff",  "sig_Mass");
+  theFit.addPdfWName("myFit", "sig" , "mggjj",  "Totti",  "sig_Mass");
                                                                                 
   // mLL PDF - background
   // theFit.addPdfWName("myFit", "bkg" , "massggnewvtx",  "Bernstein3", "bkg_Mass");    
-  theFit.addPdfWName("myFit", "bkg" , "massggnewvtx",  "Expo", "bkg_Mass");     
+  // theFit.addPdfWName("myFit", "bkg" , "massggnewvtx",  "Expo", "bkg_Mass");     
+  theFit.addPdfWName("myFit", "bkg" , "mggjj",  "Expo", "bkg_Mass");     
 
   // Load the data
   char datasetnameB[200];
-  sprintf(datasetnameB, "../HHggbb/finalizedTrees_Radion_codiceCommittato/Radion_Data2012_default_CSV.root");  
-  // sprintf(datasetnameB, "../HHggbb/finalizedTrees_Radion_cut/Radion_Data2012_default_CSV.root");  
+  sprintf(datasetnameB, "../HHggbb/finalizedTrees_Radion_presel/Radion_Data2012_default_CSV.root");  
+
+  char datasetnameS[200];
+  sprintf(datasetnameS, "../HHggbb/finalizedTrees_Radion_presel/Radion_Radion_M-300_madgraph_default_CSV.root");
 
   char treename[100];
-  if(opts.getBoolVal("dataFit")) sprintf(treename,"myTrees");  
+  sprintf(treename,"myTrees");  
 
   RooDataSet *data;
+  if(opts.getBoolVal("sgnOnlyFit")) data = getDataSet(datasetnameS,  treename);
   if(opts.getBoolVal("bkgOnlyFit")) data = getDataSet(datasetnameB,  treename);
   if(opts.getBoolVal("dataFit"))    data = getDataSet(datasetnameB,  treename);
   // data = (RooDataSet*)data->reduce("massggnewvtx>=100 && massggnewvtx<=180 && (massggnewvtx>=135 || massggnewvtx<=115) && mjj>0 && mjj<300");
@@ -174,19 +193,22 @@ void PlotGG(int nbins=19) {
 
   // Initialize the fit...
   char configfilename[200];
+  if(opts.getBoolVal("sgnOnlyFit"))    theFit.initialize("fitres/fitSignal.config");  
   if(opts.getBoolVal("bkgOnlyFit"))    theFit.initialize("fitres/fitBackground.config");  
   if(opts.getBoolVal("dataFit"))       theFit.initialize("fitres/fitData.config");    
 
   // Plots
   TCanvas *c = new TCanvas("c","fitResult");
   char rootfilename[200];
+  if(opts.getBoolVal("sgnOnlyFit"))    sprintf(rootfilename,"fitres/mGGSignal.root");  
   if(opts.getBoolVal("bkgOnlyFit"))    sprintf(rootfilename,"fitres/mGGBackground.root");  
   if(opts.getBoolVal("dataFit"))       sprintf(rootfilename,"fitres/mGGData.root");    
 
   TFile *output = new TFile(rootfilename,"RECREATE");   
 
   //////////////////////////////
-  RooRealVar* Var = theFit.RealObservable("massggnewvtx");
+  // RooRealVar* Var = theFit.RealObservable("massggnewvtx");  // chiara
+  RooRealVar* Var = theFit.RealObservable("mggjj");            // chiara
   double min=Var->getMin();
   double max=Var->getMax();
   RooPlot *plot = Var->frame(min,max,nbins);
@@ -198,9 +220,9 @@ void PlotGG(int nbins=19) {
   RooAbsPdf *thePdf = theFit.getPdf("myFit");
 
   // plot background in the full range, but normalized to fitted ranges - chiara
-  // thePdf->plotOn(plot, RooFit::LineColor(kBlack));
-  thePdf->plotOn(plot, RooFit::Range("Full"), RooFit::NormRange("R1,R2"),RooFit::LineColor(kBlue));
-  thePdf->plotOn(plot, RooFit::Range("R1,R2"), RooFit::NormRange("R1,R2"),RooFit::LineColor(kRed));
+  thePdf->plotOn(plot, RooFit::LineColor(kBlack));
+  // thePdf->plotOn(plot, RooFit::Range("Full"), RooFit::NormRange("R1,R2"),RooFit::LineColor(kBlue));
+  // thePdf->plotOn(plot, RooFit::Range("R1,R2"), RooFit::NormRange("R1,R2"),RooFit::LineColor(kRed));
 
   // plot (dashed) the bkg component
   // double Ns = theFit.getRealPar("N_sig")->getVal();
@@ -211,6 +233,7 @@ void PlotGG(int nbins=19) {
   plot->SetYTitle("Events");
   plot->Draw();
   char epsfilename[200];
+  if(opts.getBoolVal("sgnOnlyFit")) c->SaveAs("fitres/mGGSignal.png");  
   if(opts.getBoolVal("bkgOnlyFit")) c->SaveAs("fitres/mGGBackground.png");  
   if(opts.getBoolVal("dataFit"))    c->SaveAs("fitres/mGGData.png");  
   plot->Write();
